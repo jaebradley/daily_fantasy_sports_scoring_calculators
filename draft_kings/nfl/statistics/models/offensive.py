00664319c53a41199683d19f2474ecf57abe5379
Jae Bradley <jae.b.bradley@gmail.com>
2021-08-03 22:45:06 -0400

    NFL model use dataclasses (#4)

--- draft_kings/nfl/statistics/models/offensive.py
+++ draft_kings/nfl/statistics/models/offensive.py
@@ -1,70 +1,76 @@
+from dataclasses import dataclass
+
+
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
 class YardageStatistics:
-    def __init__(self, passing: int, rushing: int, receiving: int):
-        self.passing = passing
-        self.rushing = rushing
-        self.receiving = receiving
+    passing: int
+    rushing: int
+    receiving: int
 
 
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
 class KickReturnTouchdownStatistics:
-    def __init__(self, punts: int, kickoffs: int, field_goals: int):
-        if 0 > punts or 0 > kickoffs or 0 > field_goals:
+    punts: int
+    kickoffs: int
+    field_goals: int
+
+    def __post_init__(self):
+        if 0 > self.punts or 0 > self.kickoffs or 0 > self.field_goals:
             raise ValueError("touchdowns cannot be negative")
 
-        self.punts = punts
-        self.kickoffs = kickoffs
-        self.field_goals = field_goals
 
-
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
 class TouchdownStatistics:
-    def __init__(self, passing: int, rushing: int, receiving: int, kick_returns: KickReturnTouchdownStatistics,
-                 fumble_recoveries: int):
-        if 0 > passing or 0 > rushing or 0 > receiving or 0 > fumble_recoveries:
+    passing: int
+    rushing: int
+    receiving: int
+    kick_returns: KickReturnTouchdownStatistics
+    fumble_recoveries: int
+
+    def __post_init__(self):
+        if 0 > self.passing or 0 > self.rushing or 0 > self.receiving or 0 > self.fumble_recoveries:
             raise ValueError("touchdowns cannot be negative")
 
-        self.passing = passing
-        self.rushing = rushing
-        self.receiving = receiving
-        self.kick_returns = kick_returns
-        self.fumble_recoveries = fumble_recoveries
 
-
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
 class TwoPointConversionStatistics:
-    def __init__(self, thrown: int, rushed: int, caught: int):
-        if 0 > thrown or 0 > rushed or 0 > caught:
+    thrown: int
+    rushed: int
+    caught: int
+
+    def __post_init__(self):
+        if 0 > self.thrown or 0 > self.rushed or 0 > self.caught:
             raise ValueError("conversions cannot be negative")
 
-        self.thrown = thrown
-        self.rushed = rushed
-        self.caught = caught
 
-
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
 class ConversionStatistics:
-    def __init__(self, two_point: TwoPointConversionStatistics):
-        self.two_point = two_point
+    two_point: TwoPointConversionStatistics
 
 
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
 class ScoringStatistics:
-    def __init__(self, touchdowns: TouchdownStatistics, conversions: ConversionStatistics):
-        self.touchdowns = touchdowns
-        self.conversions = conversions
+    touchdowns: TouchdownStatistics
+    conversions: ConversionStatistics
 
 
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
 class TurnoverStatistics:
-    def __init__(self, interceptions: int, fumbles_lost: int):
-        if 0 > interceptions or 0 > fumbles_lost:
+    interceptions: int
+    fumbles_lost: int
+
+    def __post_init__(self):
+        if 0 > self.interceptions or 0 > self.fumbles_lost:
             raise ValueError("turnovers cannot be negative")
 
-        self.interceptions = interceptions
-        self.fumbles_lost = fumbles_lost
 
-
+@dataclass(init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=True)
 class OffensiveStatistics:
-    def __init__(self, yards: YardageStatistics, scoring: ScoringStatistics, turnovers: TurnoverStatistics,
-                 receptions: int):
-        if 0 > receptions:
-            raise ValueError("receptions cannot be negative")
+    yards: YardageStatistics
+    scoring: ScoringStatistics
+    turnovers: TurnoverStatistics
+    receptions: int
 
-        self.yards = yards
-        self.scoring = scoring
-        self.turnovers = turnovers
-        self.receptions = receptions
+    def __post_init__(self):
+        if 0 > self.receptions:
+            raise ValueError("receptions cannot be negative")
